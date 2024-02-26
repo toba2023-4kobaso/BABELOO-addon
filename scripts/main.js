f4f6a5385195bef6dfb2ff8fcb928086543e7edf
Kobaso <soya.k0622@icloud.com>
2024-02-27 01:04:18 +0900

    language,オブジェクトの文字列表現が終わってない

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 import {world} from "@minecraft/server"
+
+world.events.beforeChat.subscribe(event => {
+    event.cancel = true;
+    event.sender.runCommand("title @s times 0 0 0");
+
+    
+    const obj ={
+        message: [event.messege],
+        language: ["ja" , "en"]
+    }
+
+    event.sender.runCommand("title @s title ");
+});
