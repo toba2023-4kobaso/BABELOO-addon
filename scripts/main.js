0ca17e57b5c6e0ef02c9657e225c52431aa4c886
4geniu <taketubo0714@gmail.com>
2024-02-28 01:00:45 +0900

    一部スペルミスの訂正

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 import {world} from "@minecraft/server"
 
-world.events.beforeChat.subscribe(event => {
+world.beforeEvents.chatSend.subscribe(event => {
     event.cancel = true;
-    event.sender.runCommand("title @s times 0 0 0");
+    event.sender.runCommandAsync("title @s times 0 0 0");
 
     let string;
     if(event.sender.hasTag("japanese")) string = "ja";
     if(event.sender.hasTag("english")) string = "en";
 
     let obj ={
-        message: event.messege,
+        message: event.message,
         language: string
     }
 
     let strobj = JSON.stringify(obj);
 
-    event.sender.runCommand(`title @s title ${strobj}`);
+    event.sender.runCommandAsync(`title @s title ${strobj}`);
 });
